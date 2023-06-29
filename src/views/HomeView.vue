da7c2a49d38a01f4a0b5283a3f4f3ae89125574d
S1bu <Sibusisodyan@gmail.com>
2023-06-29 15:44:13 +0200

    committing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div id="home" class="container">
+<form class="form" @submit.prevent="displayData">
+  <div>
+    <input class="form-control" v-model="firstName"  type="text" placeholder="Enter your first name" required>
   </div>
+  <div>
+    <input class="form-control my-3" v-model="lastName" type="text" placeholder="Enter your last name" required>
+  </div>
+  <div>
+
+    <label class="form-control mb-3" id="output"> </label>
+  </div>
+  <div>
+     <button class="btn btn-success" type="button" @click="displayData">Submit</button>
+  </div>
+</form>
+ </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+ 
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+   
+  },
+  data(){
+    return{
+      firstName:'',
+      lastName: ''
+    }
+  },
+  methods:{
+    displayData(){
+      document.querySelector('#output').textContent=`${this.firstName} ${this.lastName}`
+    }
   }
 }
 </script>
